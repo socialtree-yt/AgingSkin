2316615237b6df9505a9d4b7f60366defa888ade
socialtree-yt <73478273+socialtree-yt@users.noreply.github.com>
2024-12-06 10:43:03 +0800

    Delete scRNA-seq directory

--- scRNA-seq/02.1-KC_subanalysis/05.4-SCENIC/pyscenic_input.R
+++
@@ -1,8 +0,0 @@
-### Initialize settings
-library(Seurat) 
-data=readRDS(file = "../05.2-subtype/scRNA_annodata.KC.rds")
-data=subset(data,idents=c('SC','BC','SC_BC','GC_SC')) ###细胞数太少的cluster也不要
-t=t(as.matrix(data@assays$RNA@counts))
-head(t)
-dim(t)
-write.csv(t(as.matrix(data@assays$RNA@counts)),file = "RNA_count.csv")
